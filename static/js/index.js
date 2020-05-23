4bfabb68b79d3f04e06e646edee2f37da0443af5
Saeeda Choudhury <saeedakc1@gmail.com>
2020-05-23 15:08:34 -0400

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,61 +1,179 @@
-new WOW().init();
+//new WOW().init();
 
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 660;
+// // Define SVG area dimensions
+// var svgWidth = 960;
+// var svgHeight = 660;
 
-// Define the chart's margins as an object
-var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
-};
+// // Define the chart's margins as an object
+// var chartMargin = {
+//   top: 30,
+//   right: 30,
+//   bottom: 30,
+//   left: 30
+// };
 
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+// // Define dimensions of the chart area
+// var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+// var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+// // Select body, append SVG area to it, and set the dimensions
+// var svg = d3
+//   .select("body")
+//   .append("svg")
+//   .attr("height", svgHeight)
+//   .attr("width", svgWidth);
 
-// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
-// to the margins set in the "chartMargin" object.
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+// // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// // to the margins set in the "chartMargin" object.
+// var chartGroup = svg.append("g")
+//   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-// Load data from hours-of-tv-watched.csv
-d3.csv("/static/data/age_totals.csv").then(function(ageData) {
+// // Load data
+// d3.csv("/static/data/age_totals.csv").then(function(ageData) {
 
-  // Print the tvData
-  console.log(ageData);
+//   // Print data
+//   console.log(ageData);
 
-  // Cast the hours value to a number for each piece of tvData
-  ageData.forEach(function(data) {
-    data.hours = +data.hours;
-  });
+// //   // Cast the hours value to a number for each piece of tvData
+// //   ageData.forEach(function(data) {
+// //     data.hours = +data.hours;
+// //   });
 
-  var barSpacing = 10; // desired space between each bar
-  var scaleY = 10; // 10x scale on rect height
+//   var barSpacing = 10; // desired space between each bar
+//   var scaleY = 10; // 10x scale on rect height
 
-  // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
-  var barWidth = (chartWidth - (barSpacing * (ageData.length - 1))) / ageData.length;
+//   // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
+//   var barWidth = (chartWidth - (barSpacing * (ageData.length - 1))) / ageData.length;
 
-  // @TODO
-  // Create code to build the bar chart using the tvData.
-  chartGroup.selectAll(".bar")
-    .data(ageData)
-    .enter()
-    .append("rect")
-    .classed("bar", true)
-    .attr("width", d => barWidth)
-    .attr("height", d => d.hours * scaleY)
-    .attr("x", (d, i) => i * (barWidth + barSpacing))
-    .attr("y", d => chartHeight - d.hours * scaleY);
-}).catch(function(error) {
-  console.log(error);
+//   // @TODO
+//   // Create code to build the bar chart using the tvData.
+//   chartGroup.selectAll(".bar")
+//     .data(ageData)
+//     .enter()
+//     .append("rect")
+//     .classed("bar", true)
+//     .attr("width", d => barWidth)
+//     .attr("height", d => d.hours * scaleY)
+//     .attr("x", (d, i) => i * (barWidth + barSpacing))
+//     .attr("y", d => chartHeight - d.hours * scaleY);
+// }).catch(function(error) {
+//   console.log(error);
+// });
+
+
+
+
+
+
+
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// var x0 = d3.scale.ordinal()
+//     .rangeRoundBands([0, width], .1);
+
+// var x1 = d3.scale.ordinal();
+
+// var y = d3.scale.linear()
+//     .range([height, 0]);
+
+// var xAxis = d3.svg.axis()
+//     .scale(x0)
+//     .tickSize(0)
+//     .orient("bottom");
+
+// var yAxis = d3.svg.axis()
+//     .scale(y)
+//     .orient("left");
+
+// var color = d3.scale.ordinal()
+//     .range(["#ca0020","#f4a582","#d5d5d5","#92c5de","#0571b0"]);
+
+var svg = d3.select('body').append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("/static/data/age_totals.csv", function(error, data) {
+
+  var categoriesNames = data.map(function(d) { return d.categorie; });
+  var rateNames = data[0].values.map(function(d) { return d.rate; });
+
+  x0.domain(categoriesNames);
+  x1.domain(rateNames).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(data, function(categorie) { return d3.max(categorie.values, function(d) { return d.value; }); })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .style('opacity','0')
+      .call(yAxis)
+  .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .style('font-weight','bold')
+      .text("Value");
+
+  svg.select('.y').transition().duration(500).delay(1300).style('opacity','1');
+
+  var slice = svg.selectAll(".slice")
+      .data(data)
+      .enter().append("g")
+      .attr("class", "g")
+      .attr("transform",function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
+
+  slice.selectAll("rect")
+      .data(function(d) { return d.values; })
+  .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) { return x1(d.rate); })
+      .style("fill", function(d) { return color(d.rate) })
+      .attr("y", function(d) { return y(0); })
+      .attr("height", function(d) { return height - y(0); })
+      .on("mouseover", function(d) {
+          d3.select(this).style("fill", d3.rgb(color(d.rate)).darker(2));
+      })
+      .on("mouseout", function(d) {
+          d3.select(this).style("fill", color(d.rate));
+      });
+
+  slice.selectAll("rect")
+      .transition()
+      .delay(function (d) {return Math.random()*1000;})
+      .duration(1000)
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
+
+  //Legend
+  var legend = svg.selectAll(".legend")
+      .data(data[0].values.map(function(d) { return d.rate; }).reverse())
+  .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d,i) { return "translate(0," + i * 20 + ")"; })
+      .style("opacity","0");
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", function(d) { return color(d); });
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) {return d; });
+
+  legend.transition().duration(500).delay(function(d,i){ return 1300 + 100 * i; }).style("opacity","1");
+
 });
